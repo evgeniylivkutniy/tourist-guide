58cab3685d5aaa71747e9ffcf877db94954d3d90
evgeniylivkutniy <evgeniy.livkutniy@gmail.com>
2021-11-09 21:37:24 +0200

    Update script.js

--- TouristProject/script.js
+++ TouristProject/script.js
@@ -1,7 +1,24 @@
+let findMeButton = document.getElementById('findMe');
+
+const getMyPosition = () => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+
+        // Текущие координаты.
+        return x = position.coords.latitude,
+            y = position.coords.longitude;
+
+
+    })
+}
+findMeButton.addEventListener('click', getMyPosition)
+
+
+
 function myMap() {
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 50, lng: 26 },
-        zoom: 8
+        getMyPosition,
+        center: { lat: parseFloat(x), lng: parseFloat(y) },
+        zoom: 12
     });
 }
-
